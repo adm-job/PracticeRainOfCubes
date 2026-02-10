7ae4102958529f8a589a2f84df3922ee666c1504
Stanislav <adm.job@yandex.ru>
2026-02-11 01:01:59 +0500

    modifi script Cube

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,7 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MeshRenderer))]
 public class Cube : MonoBehaviour
 {
-
+    public MeshRenderer Renderer { get; private set; }
+    public Rigidbody Rigidbody { get; private set; }
+    
+    private void Awake()
+    {
+        Renderer = GetComponent<MeshRenderer>();
+        Rigidbody = GetComponent<Rigidbody>();
+    }
 }
+
