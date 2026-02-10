1275084930b0cea45facef84b19ffba38b124e46
Stanislav <adm.job@yandex.ru>
2026-02-11 01:14:02 +0500

    point save

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Rain : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Spawner spawner;
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
+    [SerializeField] Cube Cube;
 }
