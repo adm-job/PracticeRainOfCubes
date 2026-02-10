150aa0bc0dac0c8e7b597ce9c514422e26ca444f
Stanislav <adm.job@yandex.ru>
2026-02-11 01:45:28 +0500

    point  save

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,4 +5,23 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] Cube Cube;
+    
+    private float _rainHeight = 50f;
+
+    private void Start()
+    {
+        CreatePoint();
+    }
+
+    private void CreatePoint()
+    {
+        GameObject point = new("PointRain");
+        point.transform.position = new Vector3(0, _rainHeight, 0);
+    }
+
 }
+
+
+
+
+
