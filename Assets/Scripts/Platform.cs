87129ae190e3982a12a92df3890b83d0661f3f92
Stanislav <adm.job@yandex.ru>
2026-02-17 23:45:28 +0500

    del rain script

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(BoxCollider))]
 public class Platform : MonoBehaviour
 { 
